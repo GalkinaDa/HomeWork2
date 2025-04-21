3c728ff104cbd307f2357a7b2b0a838f34f04b66
Galkina <darja.galkin@gmail.com>
2025-04-21 11:36:36 +0300

    Сalculate bonus miles

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 18_534;
+        int rubles = 20;
+        int bonus = price / rubles;
+        System.out.println("Бонусные мили: " + bonus);
+    }
+}
